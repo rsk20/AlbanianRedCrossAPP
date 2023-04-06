9ae10f9e0e8a5376d63c652280cad607bba3c9a9
rsk20 <111432617+rsk20@users.noreply.github.com>
2023-04-07 00:47:00 +0200

    viewpager2 added

--- app/src/main/java/com/resia/albanianredcross/MainActivity.kt
+++ app/src/main/java/com/resia/albanianredcross/MainActivity.kt
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 
+
+import androidx.viewpager2.widget.ViewPager2
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+        getSupportActionBar()!!.hide();
         setContentView(R.layout.activity_main)
+
+
     }
+
+    /*val viewPager: ViewPager2 = findViewById(R.id.ViewPager2)*/
+
 
 
 }

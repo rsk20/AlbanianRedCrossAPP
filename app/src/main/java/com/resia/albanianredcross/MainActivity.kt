db4379ccf8a5cbe1129cf7884f6fae87447bcf2f
rsk20 <111432617+rsk20@users.noreply.github.com>
2023-04-07 01:32:18 +0200

    viewpager2 added

--- app/src/main/java/com/resia/albanianredcross/MainActivity.kt
+++ app/src/main/java/com/resia/albanianredcross/MainActivity.kt
@@ -14,6 +14,43 @@ class MainActivity : AppCompatActivity() {
         getSupportActionBar()!!.hide();
         setContentView(R.layout.activity_main)
 
+        class MainActivity : AppCompatActivity() {
+            private val images = listOf(
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3
+            )
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+
+                viewPager2.adapter = ImageAdapter(images)
+            }
+
+            private class ImageAdapter(private val images: List<Int>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+                    val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+                    return ImageViewHolder(view)
+                }
+
+                override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+                    holder.bind(images[position])
+                }
+
+                override fun getItemCount(): Int = images.size
+
+                inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+                    private val imageView: ImageView = itemView.findViewById(R.id.imageView)
+
+                    fun bind(imageResId: Int) {
+                        imageView.setImageResource(imageResId)
+                    }
+                }
+            }
+        }
+
+
 
     }
 

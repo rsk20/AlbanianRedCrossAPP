d62fcd9dadbefcccc96a090dcd6e1109504b35a8
rsk20 <111432617+rsk20@users.noreply.github.com>
2023-04-17 22:06:57 +0200

    status bar color

--- app/src/main/java/com/resia/albanianredcross/MainActivity.kt
+++ app/src/main/java/com/resia/albanianredcross/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.resia.albanianredcross
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import androidx.core.content.ContextCompat
+
 
 
 import androidx.viewpager2.widget.ViewPager2
@@ -14,41 +17,11 @@ class MainActivity : AppCompatActivity() {
         getSupportActionBar()!!.hide();
         setContentView(R.layout.activity_main)
 
-        class MainActivity : AppCompatActivity() {
-            private val images = listOf(
-                R.drawable.image1,
-                R.drawable.image2,
-                R.drawable.image3
-            )
-
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContentView(R.layout.activity_main)
-
-                viewPager2.adapter = ImageAdapter(images)
-            }
-
-            private class ImageAdapter(private val images: List<Int>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
-                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-                    val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
-                    return ImageViewHolder(view)
-                }
-
-                override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-                    holder.bind(images[position])
-                }
-
-                override fun getItemCount(): Int = images.size
-
-                inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-                    private val imageView: ImageView = itemView.findViewById(R.id.imageView)
-
-                    fun bind(imageResId: Int) {
-                        imageView.setImageResource(imageResId)
-                    }
-                }
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.purple_200)
         }
+
+
 
 
 
